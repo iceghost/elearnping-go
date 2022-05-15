cd0f51a2a8ac7db7369d40fb76d36d68251c53a9
iceghost <khangemail22@gmail.com>
2022-05-15 23:39:24 +0700

    Hide token in logging

--- moodle/http.go
+++ moodle/http.go
@@ -16,7 +16,13 @@ var logger = log.New(os.Stdout, "[base fetch]", log.Ldate|log.Ltime)
 // rate limited fetch function
 func Fetch(url url.URL) (body io.ReadCloser, err error) {
 	rl.Take()
-	logger.Printf("Fetch %s", url.String())
+	{
+		logURL := url
+		values := logURL.Query()
+		values.Set("wstoken", "")
+		logURL.RawQuery = values.Encode()
+		logger.Printf("Fetch %s", logURL.String())
+	}
 	if res, err := http.Get(url.String()); err != nil {
 		return nil, err
 	} else {
